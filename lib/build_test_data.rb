1b7f6194193dad00c8564b17ca8650ffd39c50cb
Mike Stone <mikestone14@gmail.com>
2014-04-01 13:36:52 -0400

    rake task has teacher membership

--- lib/build_test_data.rb
+++ lib/build_test_data.rb
@@ -5,8 +5,10 @@ class TestData
     Course.delete_all
     Unit.delete_all
     Lesson.delete_all
+    mike = User.find_by_first_name_and_last_name('Mike', 'Stone')
     10.times do |i|
       course = Course.create!(title: "#{Faker::Lorem.word}", subtitle: "#{Faker::Lorem.sentence}", url: "//www.youtube.com/embed/PshZbUfGV6M", description: "#{Faker::Lorem.sentence}", sport: SPORTS.sample)
+      Membership.create!(user_id: mike.id, course_id: course.id, role: 'teacher')
       3.times do |j|
         unit = Unit.create!(unit_number: (j + 1), title: "#{Faker::Lorem.word}", description: "#{Faker::Lorem.sentence}", course_id: course.id)
         3.times do |l|
